56843af7b7ed27adec12eac944c0a10bddaacaa5
Karneyaprojects <karneya.r02@gmail.com>
2025-07-29 16:29:15 +0530

    First Commit

--- routes/Payment.js
+++ routes/Payment.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const crypto = require('crypto');
+const db = require('../index');
 
 
 router.get('/payment-status/:transactionId', async (req, res) => {
